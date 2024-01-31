d5bfc33a8062fcc4e00610a23d9154308e5cf187
Brandon Seah <brandon.seah@thuenen.de>
2024-01-31 14:11:02 +0100

    Adapt script, write JSON output
    MMseqs commands now in Snakemake rules, adapt script input arguments
    accordingly. Report output as JSON for easier parsing downstream.

--- MD.py
+++ MD.py
@@ -1,118 +1,130 @@
-# Metagenomic diversity (MD) calculator 
-# Measures size and (dis)similarity of protein clusters from 
+# Metagenomic diversity (MD) calculator
+# Measures size and (dis)similarity of protein clusters from
 # MMSeqs2 cluster and alignment outputs
 
-# Note: this code assumes you have installed MMseqs2 
-
-# Usage without random subsampling: python MD.py -i <predicted_protein-encoding_genes_file.faa> -r F 
-# With random subsampling: python MD.py -i <predicted_protein-encoding_genes_file.faa> -r T -N xxx
-# where xxx is the total number of observations to randomly subsample
-# Where users may wish to define their own MMSeqs2 E value and/or sequence identity cut-offs: python MD.py -i <predicted_protein-encoding_genes_file.faa> -r F -e xxx -I yyy
-# where xxx and yyy are the user degined inputs for these cut-offs
-
-# Written by Damien Finn, damien.finn@thuenen.de  
+# Written by Damien Finn, damien.finn@thuenen.de
 
 import os
+import json
 import argparse
 import numpy as np
-from collections import defaultdict
 import multiprocessing as mp
 import random
+from collections import defaultdict
 
-# Define input data
 
-parser = argparse.ArgumentParser(description= 'Derive a Metagenomic Diversity index from clustered amino acid sequences')
-parser.add_argument('-i', '--input-file', required = True, nargs = 1, 
-                    help = 'Amino acid sequences as .faa')
-parser.add_argument('-r', '--random-sampling', required = False, nargs = 1, 
-                    help = 'To randomly subsample observations or not, as T if randomly subsampling')
-parser.add_argument('-N', '--seq-obs', required = False, nargs = 1, type = int, 
-                    help = 'If randomly subsampling, N denotes the number of observations to subsample without replacement')
-parser.add_argument('-e', '--Eval', required = False, nargs = 1, type = str, 
-                    help = 'Redefine the MMSeqs2 minimum Evalue cutoff score')
-parser.add_argument('-I', '--identity', required = False, nargs = 1, type = str, 
-                    help = 'Redefine the MMSeqs2 minimum sequence identity cutoff score')
-args = parser.parse_args()
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Derive a Metagenomic Diversity index from clustered amino acid sequences"
+    )
+    parser.add_argument(
+        "-c",
+        "--input_clust",
+        required=True,
+        help="Path to mmseqs cluster results in TSV format",
+    )
+    parser.add_argument(
+        "-m",
+        "--input_aln",
+        required=True,
+        help="Path to mmseqs search results in .m8 format",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Path to write output file in JSON format",
+    )
+    parser.add_argument(
+        "-r",
+        "--random-sampling",
+        action="store_true",
+        help="Optional: randomly subsample observations",
+    )
+    parser.add_argument(
+        "-N",
+        "--seq-obs",
+        required=False,
+        nargs=1,
+        type=int,
+        help="""If randomly subsampling (option -r), N denotes the number of
+        observations to subsample without replacement""",
+    )
+    parser.add_argument(
+        "-T",
+        "--cpus",
+        default=int(mp.cpu_count()),
+        type=int,
+        help="Max number of CPUs (default: total available)",
+    )
+    args = parser.parse_args()
+    return args
 
-input_file = os.path.join(os.getcwd(), str(args.input_file[0]))
+args = get_args()
 
-# Run MMseqs2 
-os.system('mmseqs createdb ' + input_file + ' DB')
+# Generate a dictionary of clusters
+print(" -- Making Protein dictionary -- ")
+clustlist = []
 
-if args.Eval is None and args.identity is None: 
-    os.system('mmseqs cluster DB Clu tmp')
-elif args.Eval is not None and args.identity is None:
-    os.system('mmseqs cluster DB Clu tmp -e ' + args.Eval[0] )
-elif args.Eval is None and args.identity is not None:
-    os.system('mmseqs cluster DB Clu tmp --min-seq-id ' + args.identity[0] )
-elif args.Eval is not None and args.identity is not None:
-    os.system('mmseqs cluster DB Clu tmp -e ' + args.Eval[0] + ' --min-seq-id ' + args.identity[0] )
-
-os.system('mmseqs createtsv DB DB Clu Clu.tsv')
-os.system('mmseqs search DB DB res tmp')
-os.system('mmseqs convertalis DB DB res res.m8')
-
-input_clust = os.path.join(os.getcwd(), str('Clu.tsv'))
-input_aln = os.path.join(os.getcwd(), str('res.m8'))
-
-# Generate a dictionary of clusters 
-print(' -- Making Protein dictionary -- ')
-clustlist = [] 
-
-with open(input_clust) as file:
+with open(args.input_clust) as file:
     for l in file:
         clustlist.append(l.rstrip())
 
-if str(args.random_sampling[0])  == 'T':
+if args.random_sampling:
     rarvals = random.sample(clustlist, int(args.seq_obs[0]))
     clustlist = rarvals
 
-clusters = defaultdict(list) 
+clusters = defaultdict(list)
 
-pool = mp.Pool(mp.cpu_count())
+pool = mp.Pool(args.cpus)
+
 
 def make_clust_dict(clustlist):
     for l in clustlist:
-        col1 = l.split('\t')[0]
-        col2 = l.split('\t')[1]  
-        clusters[col1].append(col2) 
+        col1 = l.split("\t")[0]
+        col2 = l.split("\t")[1]
+        clusters[col1].append(col2)
+
 
 pool.apply_async(make_clust_dict(clustlist))
 
 # Generate a dictionary of pairwise distances
-print(' -- Collating pair-wise dissimilarities -- ')
-alnlist = [] 
+print(" -- Collating pair-wise dissimilarities -- ")
+alnlist = []
 
-with open(input_aln) as file:
+with open(args.input_aln) as file:
     for l in file:
         alnlist.append(l.rstrip())
 
 pairsim = defaultdict(list)
 
+
 def make_pairsim_dict(alnlist):
     for l in alnlist:
-        col1 = l.split('\t')[0]
-        col2 = l.split('\t')[1]
-        col3 = l.split('\t')[2]
+        col1 = l.split("\t")[0]
+        col2 = l.split("\t")[1]
+        col3 = l.split("\t")[2]
         tmp = [col2, col3]
         pairsim[col1].append(tmp)
+
 
 pool.apply_async(make_pairsim_dict(alnlist))
 
 # Now to bring them together
-print(' -- Bringing things together -- ')
+print(" -- Bringing things together -- ")
 clustdist = defaultdict(list)
 
+
 def sorter(clusters, pairsim):
-    for c in clusters.keys(): # key name
-        v = clusters[c] # values in c
+    for c in clusters.keys():  # key name
+        v = clusters[c]  # values in c
         for q in pairsim.keys():
-            if c == q: # if clusters key and pairsim key match
-                valq = pairsim[q] # pairwise values in q
+            if c == q:  # if clusters key and pairsim key match
+                valq = pairsim[q]  # pairwise values in q
                 for a in v:
                     for b in valq:
-                        if a == b[0]: # matching cluster pair with pairsim value pair
+                        if a == b[0]:  # matching cluster pair with pairsim value pair
                             clustdist[c].append(1 - float(b[1]))
+
 
 pool.apply_async(sorter(clusters, pairsim))
 
@@ -121,34 +133,33 @@ pool.join()
 
 # Derive indices from the cluster dictionary
 
-print(' -- Deriving diversity indices -- ')
+print(" -- Deriving diversity indices -- ")
 
+results = {}
 # Total Contigs
-O = len(clustlist)
-print('Total protein-encoding genes: ', O)
+results['total_protein_coding'] = len(clustlist)
 
 # Protein Richness
-P = len(clustdist)
-print('Protein Richness: ', P)
+results['protein_richness'] = len(clustdist)
 
-CDlens = [ len(k) for k in clustdist.values()] 
+CDlens = [len(k) for k in clustdist.values()]
 tmp = np.sum(CDlens)
 
 # Shannon Diversity
-H = [ (len(k)/tmp)*np.log(len(k)/tmp) for k in clustdist.values() ]
-print('Shannon Diversity: ', np.sum(H)*-1) 
+H = [(len(k) / tmp) * np.log(len(k) / tmp) for k in clustdist.values()]
+results['shannon_diversity'] = np.sum(H) * -1
 
 # Simpson Diversity
-S = [ np.square(len(k)/tmp) for k in clustdist.values() ]
-print('Simpson Evenness: ', 1 - np.sum(S))
+S = [np.square(len(k) / tmp) for k in clustdist.values()]
+results['simpson_evenness'] = 1 - np.sum(S)
 
 # Metagenomic diversity
-MD = [ (1 + (np.sum(k))/len(k)) for k in clustdist.values()]
-print('Log10 Protein Dissimilarity:', np.log10(np.sum(MD)))
-print('Metagenomic Diversity index:', (1/O)*np.sum(MD))
+MD = [(1 + (np.sum(k)) / len(k)) for k in clustdist.values()]
+results['protein_dissim_log10'] = np.log10(np.sum(MD))
+results['md_index'] = (1 / results['total_protein_coding']) * np.sum(MD)
 
-# A little Aufraumen 
-os.system('rm -rf tmp')
-os.system('rm Clu*')
-os.system('rm res*')
-os.system('rm DB*')
+if args.output:
+    with open(args.output, "w") as fh:
+        fh.write(json.dumps(results,indent=4))
+else:
+    print(json.dumps(results,indent=4))

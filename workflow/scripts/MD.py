b4fa87ff86800f22b7c1dc05d8981ddec764590d
Brandon Seah <brandon.seah@thuenen.de>
2024-02-01 13:28:49 +0100

    Use logging module

--- workflow/scripts/MD.py
+++ workflow/scripts/MD.py
@@ -8,11 +8,34 @@ import os
 import sys
 import json
 import argparse
+import logging
 import numpy as np
 import multiprocessing as mp
 import random
 from collections import defaultdict
 
+def configure_logger(log_path=None):
+    logger = logging.getLogger("log")
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+
+    if log_path:
+        print(f"Log path specified at {log_path}")
+        log_fh = logging.FileHandler(log_path)
+        log_fh.setLevel(logging.DEBUG)
+        log_fh.setFormatter(formatter)
+        logger.addHandler(log_fh)
+    else:
+        log_fh = logging.StreamHandler()
+        log_fh.setLevel(logging.DEBUG)
+        log_fh.setFormatter(formatter)
+        logger.addHandler(log_fh)
+
+    # Redirect unhandled exceptions to logger
+    def excepthook(exctype, value, traceback):
+        logger.error("Exception", exc_info=(exctype, value, traceback))
+    sys.excepthook = excepthook
+    return logger
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -22,13 +45,13 @@ def get_args():
         "-c",
         "--input_clust",
         required=True,
-        help="Path to mmseqs cluster results in TSV format",
+        help="Path to mmseqs cluster or diamond deepclust results in TSV format",
     )
     parser.add_argument(
         "-m",
         "--input_aln",
         required=True,
-        help="Path to mmseqs search results in .m8 format",
+        help="Path to mmseqs search or diamond realign results in .m8 format",
     )
     parser.add_argument(
         "-o",
@@ -65,7 +88,7 @@ def get_args():
         "--tool",
         default="mmseqs",
         type=str,
-        help="Tool used for clustering",
+        help="Tool used for clustering: mmseqs or diamond only (default: mmseqs)",
     )
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
@@ -86,12 +109,17 @@ if __name__ == "__main__":
             "tool": snakemake.params["tool"],
             "cpus": int(snakemake.threads),
         }
-        sys.stderr = open(snakemake.log[0], "w")  # redirect error msgs to log
+        logger = configure_logger(log_path=snakemake.log[0])
     except NameError:
         args = vars(get_args())
+        logger = configure_logger()
+
+    logger.info("Script called with the following arguments:")
+    for i in args:
+        logger.info("  %s : %s", i, str(args[i]))
 
     # Generate a dictionary of clusters
-    print(" -- Making Protein dictionary -- ", file=sys.stderr)
+    logger.info(" -- Making Protein dictionary -- ")
     clustlist = []
 
     with open(args["input_clust"]) as file:
@@ -117,7 +145,7 @@ if __name__ == "__main__":
     pool.apply_async(make_clust_dict(clustlist))
 
     # Generate a dictionary of pairwise distances
-    print(" -- Collating pair-wise dissimilarities -- ", file=sys.stderr)
+    logger.info(" -- Collating pair-wise dissimilarities -- ")
     alnlist = []
 
     with open(args["input_aln"]) as file:
@@ -141,7 +169,7 @@ if __name__ == "__main__":
     pool.apply_async(make_pairsim_dict(alnlist))
 
     # Now to bring them together
-    print(" -- Bringing things together -- ", file=sys.stderr)
+    logger.info(" -- Bringing things together -- ")
     clustdist = defaultdict(list)
 
     def sorter(clusters, pairsim):
@@ -152,7 +180,8 @@ if __name__ == "__main__":
                     id_val = pairsim[(c, a)]  # pairwise values in q
                     clustdist[c].append(1 - float(id_val))
                 else:
-                    print(f"Cluster reference-query pair {c}, {a} not present in pairwise alignment results!", file=sys.stderr)
+                    logger.warning("Cluster reference-query pair %s, %s not present in pairwise alignment results!", c, a)
+                    pass
 
     pool.apply_async(sorter(clusters, pairsim))
 
@@ -161,7 +190,7 @@ if __name__ == "__main__":
 
     # Derive indices from the cluster dictionary
 
-    print(" -- Deriving diversity indices -- ", file=sys.stderr)
+    logger.info(" -- Deriving diversity indices -- ")
 
     results = {}
     # Total Contigs
@@ -192,4 +221,4 @@ if __name__ == "__main__":
     else:
         print(json.dumps(results, indent=4), file=sys.stdout)
 
-    print("done", file=sys.stderr)
+    logger.info("Done")
